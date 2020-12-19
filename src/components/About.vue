00ad42353da528d0c9eddb6783c9ae4d4d1ade5b
Cera White <cwhite@anigramsproductions.com>
2020-12-18 20:44:34 -0800

    Update Background/About text

--- src/components/About.vue
+++ src/components/About.vue
@@ -5,29 +5,27 @@
 
       <p>
         I graduated from Drury University in 2012 with a major in Computer Information Systems.
-        For two years, I worked as an inventory expert at RGIS, where I learned a great deal
-        about hard work, teamwork, and responsibility, while providing clients with accurate
-        and efficient annual inventory audits. Before that, I held a brief internship at Meyer
-        Accounting and Tax, LLC, where I worked with them to gather information regarding their
-        needs and concerns, and developed a new database and company website to meet their
-        specifications.
+        Although I come from a family of developers, I didn't become interested in game design and
+        programming until my freshman year of college, when I tried my hand at designing my own
+        computer game and absolutely fell in love. It was so exciting to build the logic piece by piece,
+        find solutions to the various problems I came across, and watch my very own creation come to life.
+        I've learned a great deal from designing my own games, including how to research the market,
+        collaborate with a team, and design a user-friendly interface, and of course I've also benefited
+        from the hands-on programming experience.
       </p>
 
       <p>
-        In 2016, I graduated from Centriq Training where I trained to become a .NET developer.
-        Although I come from a family of developers, I didn't become interested in programming
-        until my freshman year of college, when I tried my hand at designing my own computer game
-        and absolutely fell in love. It was so exciting to build the logic piece by piece,
-        find solutions to the various problems I came across, and watch my very own creation come to
-        life. I've learned a great deal from designing my own games, including how to research the
-        market, collaborate with a team, and design a user-friendly interface, and of course I've
-        also benefited from the hands-on programming experience.
+        In 2016, I graduated from Centriq Training where I trained to become a .NET developer. Since then,
+        I've been working as a software engineer focusing on developing and maintaining complex web applications.
+        I'm currently a Principal Engineer/Architect at MSTS, which involves working on independent and
+        team-based projects to support internal business users as well as external clients and their users.
       </p>
 
       <p>
-        Currently, I'm working as a Principal Engineer/Architect at MSTS. My job involves working on
-        both independent and team-based development projects to support internal business users as
-        well as external clients.
+        Looking to get back to my roots in game design, I started taking classes at CG Spectrum, where I'm
+        learning the fundamentals of game design from industry professionals and getting practical experience
+        designing and prototyping my own games. I'm currently in the market for a level/world or technical designer
+        position in the greater Seattle, WA area.
       </p>
     </div>
   </div>
